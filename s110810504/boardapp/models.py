f1f21c1572236379614bbeb182c7e876f73e83ce
rick <rick20001010@gmail.com>
2022-05-11 13:33:40 +0800

    20220511

--- s110810504/boardapp/models.py
+++ s110810504/boardapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class BoardUnit(models.Model):
     bname = models.CharField(max_length=20,null= False)
     bgender = models.CharField(max_length =2 ,default = 'm' , null = False)
-    bsubject = models.CharField(max_lenth=100,null = False)
+    bsubject = models.CharField(max_length=100,null = False)
     btime = models.DateTimeField(auto_now = True)
     bmail = models.EmailField(max_length=100,blank = True , default='')
     bweb = models.CharField(max_length=200,blank = True, default='')
